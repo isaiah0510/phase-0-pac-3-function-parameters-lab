35119cef339858d8adc66648c8bd9a8bf23762f6
Isaiah Ware <Isaiah.ware0510@gmail.com>
2022-12-30 17:53:02 -0700

    commit tree 6d494eae53dfe2cdf36bf39da0dfe10b92f67c41

---
+++ index.js
@@ -0,0 +1,30 @@
+function introductionAki(Hi, Aki) {
+    console.log('${greeting}, my name is ${firstName}');
+
+indtroductionAki("Hi","Aki")
+}
+
+function introductionAki(greeting, firstName) {
+    console.log('${greeting}, my name is ${firstName}');
+
+indtroductionAki("Hi","Aki") // "Hi, my name is Aki"
+}
+
+function introductionAki(greeting, firstName) {
+    console.log("firstName: ", Aki);
+    console.log("greeting: ", Hi);
+    console.log('${greeting}, my name is ${firstName}');
+
+indtroductionAki("Hi","Aki")
+}
+
+function introduction(firstName) {
+    console.log('Hi, my name is Aki.');
+
+introduction("Aki");
+}
+
+introductionWithLanguage(name, language) {
+    console.log(`${name}, ${language}!`);
+}
+introductionWithLanguage("Aki","Ember.js"); // "Hi, my name is Aki and I am learning to program in Ember.js."
